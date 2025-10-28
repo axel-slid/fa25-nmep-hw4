b67aaba5880ffc0534f776801aed1eabfe22bd90
Alex Dils <123800824+axel-slid@users.noreply.github.com>
2025-10-27 21:23:33 -0700

    tokenizer

--- seq2seq/tokenizer/character_tokenizer.py
+++ seq2seq/tokenizer/character_tokenizer.py
@@ -4,7 +4,7 @@ import torch
 
 
 class CharacterTokenizer(Tokenizer):
-    def __init__(self, verbose: bool = False):
+    def __init__(self, verbose: bool = True):
         """
         Initializes the CharacterTokenizer class for French to English translation.
         If verbose is True, prints out the vocabulary.
@@ -22,46 +22,32 @@ class CharacterTokenizer(Tokenizer):
         # Normally, we iterate through the dataset and find all unique characters. To simplify things,
         # we will use a fixed set of characters that we know will be present in the dataset.
         self.characters = """aàâæbcçdeéèêëfghiîïjklmnoôœpqrstuùûüvwxyÿz0123456789,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}’•–í€óá«»… º◦©ö°äµ—ø­·òãñ―½¼γ®⇒²▪−√¥£¤ß´úª¾є™，ﬁõ  �►□′″¨³‑¯≈ˆ§‰●ﬂ⇑➘①②„≤±†✜✔➪✖◗¢ไทยếệεληνικαåşıруский 한국어汉语ž¹¿šćþ‚‛─÷〈¸⎯×←→∑δ■ʹ‐≥τ;∆℡ƒð¬¡¦βϕ▼⁄ρσ⋅≡∂≠π⎛⎜⎞ω∗"""
-        
-        for i, char in enumerate(self.vocab):
+        for i, char in enumerate(self.characters):
             self.vocab[char] = i
-
+    
         if verbose:
             print("Vocabulary:", self.vocab)
-
-        # raise NotImplementedError("Need to implement vocab initialization")
 
     def encode(self, text: str) -> torch.Tensor:
 
         tokens = []
 
-        for c in text.lower():
-            if c in self.vocab:
-                tokens.append(c)
+        for char in text.lower(): 
+            if char in self.vocab:
+                tokens.append(self.vocab[char])
             else:
-                raise ValueError("you have a char that is not in the valid chars")
-            
+                raise ValueError("not in vocab")
+
         return torch.tensor(tokens)
 
-
-        #raise NotImplementedError(
-        #    "Need to implement encoder that converts text to tensor of tokens."
-        #)
-
     def decode(self, tokens: torch.Tensor) -> str:
-
-        diht = {v: k for k, v in self.vocab.items()}
-
+        reverse = {v: k for k, v in self.vocab.items()}
         decoded = []
 
-        for t in tokens:
-            if t.item() in diht:
-                decoded.append(diht[t.item()])
+        for token in tokens:
+            if token.item() in reverse:
+                decoded.append(reverse[token.item()])
             else:
-                raise ValueError("you have a char that is not in the valid chars")
+                raise ValueError("not in vocab")
 
-
-
-        #raise NotImplementedError(
-        #    "Need to implement decoder that converts tensor of tokens to text."
-        #)
+        return "".join(decoded)
